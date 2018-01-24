b605e9993876eaba07a031a26cf4148f837658ec
Adrià Freixas <adrifre7@hotmail.com>
2018-01-24 20:45:19 +0100

    Spare calculation

--- BowlingKata/Bowling/Game.cs
+++ BowlingKata/Bowling/Game.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Linq;
 namespace BowlingKata.Bowling
 {
     public class Game
@@ -22,11 +22,60 @@ namespace BowlingKata.Bowling
             int _score =300;
             if(Rolls.Length > 12)
             {
-                _score = 150;
+                if (!Rolls.Contains('/') && !Rolls.Contains('X'))
+                {
+                    if (Rolls.Contains('-'))
+                    {
+                        List<char> rolls = Rolls.ToList<char>();
+                        List<int> scoreList = (from roll in rolls where roll != '-' select int.Parse(roll+"")).ToList();
+                        _score = scoreList.Sum();
+                    }
+                }
+                else
+                {
+                    if (Rolls.Contains('-'))
+                    {
+
+                        _score = processScore(Rolls);
+                    }
+                    else
+                    {
+                        _score = 150;
+                    }
+                    
+                   
+                }
             }
             //TODO figures score form Rolls
 
             return _score;
+        }
+        private int processScore(string gamerolls)
+        {
+            int result = 0;
+            bool wasSpare = false;
+            foreach (var roll in gamerolls.ToList())
+            {
+                if(roll == '/')
+                {
+                    result += 10;
+                    wasSpare = true;
+                }
+                else if(roll =='-')
+                {
+                    result += 0;
+                }
+                else if(wasSpare)
+                {
+                    int partialRes = 0;
+                    if(int.TryParse(roll.ToString(), out partialRes))
+                    {
+                        result += partialRes;
+                    }
+                    wasSpare = false;
+                }
+            }
+            return result;
         }
     }
 }
--- BowlingKataUT/GameUT.cs
+++ BowlingKataUT/GameUT.cs
@@ -22,10 +22,31 @@ namespace BowlingKataUT
             Assert.IsTrue(_game.Score == extpected);
         }
         [TestMethod]
-        public void GivenTwentyRollsPair9MissScoreShouldBe150 ()
+        public void GivenTwentyRollsPair9MissScoreShouldBe90 ()
         {
             _game.Rolls = "9-9-9-9-9-9-9-9-9-9-";
+            int extpected = 90;
+            Assert.IsTrue(_game.Score == extpected);
+        }
+        [TestMethod]
+        public void GivenTwentyRollsPair1Miss9MissScoreShouldBe50()
+        {
+            _game.Rolls = "1-9-1-9-1-9-1-9-1-9-";
+            int extpected = 50;
+            Assert.IsTrue(_game.Score == extpected);
+        }
+        [TestMethod]
+        public void GivenTwentyOneRollsPair5spareScoreShouldBe150()
+        {
+            _game.Rolls = "5/5/5/5/5/5/5/5/5/5/5";
             int extpected = 150;
+            Assert.IsTrue(_game.Score == extpected);
+        }
+        [TestMethod]
+        public void GivenTwentyRollsPaSpare1MissShouldBe55()
+        {
+            _game.Rolls = "5/1-5/1-5/1-5/1-5/1-";
+            int extpected = 55;
             Assert.IsTrue(_game.Score == extpected);
         }
     }
