16756c163fa268791f1168a4c973ab3bfff5c3c6
Adrià Freixas <adrifre7@hotmail.com>
2018-01-24 19:23:53 +0100

    Add project files.

---
+++ BowlingKataUT/GameUT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingKataUT
+{
+    [TestClass]
+    public class GameUT
+    {
+        [TestMethod]
+        public void DummyTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ BowlingKataUT/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BowlingKataUT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BowlingKataUT")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("87491d37-7b5a-441b-a848-c66a32b21035")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
